48622bd2a6bd6b4b3d9d214fe2ad0312c57cefd6
Swipe039 <24020318@vnu.edu.vn>
2025-10-03 01:27:24 +0700

    Update Ball.java

--- core/src/main/java/anhtuannguyen/oop/Ball.java
+++ core/src/main/java/anhtuannguyen/oop/Ball.java
@@ -2,44 +2,50 @@ package anhtuannguyen.oop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Ball {
-    // Thông
-    // Toạ độ của bóng
-    // Hình ảnh của bóng
-    // Kích thước
+    // Các thuộc tính của quả bóng
+    private Texture texture;
+    public int Vx = 300;
+    public int Vy = 300;
+    public double x = main.WORLD_W/2; //Cần chuyển hàng số kích thước cửa sổ thành biến static
+    public double y = main.WORLD_H/2;
+    public double dx = 0;
+    public double dy = 0;
 
-    // Khởi tạo bóng
-    Ball(int _x, int _y) {
-
-    }
-
+    // Tạo bóng
     public void create() {
-
+        texture = new Texture("ball.png");
+        
     }
 
     // Bóng di chuyển
     public void Move() {
-
+        float dt = Gdx.graphics.getDeltaTime();
+        dx = Vx * dt;
+        dy = Vy * dt;
+        x += dx;
+        y += dy;
     }
-
-    // Xuất ra màn hình
-    public void render() {
-
-    }
-
-    // Chạm vào vật thể và thay đổi chi hướng chuyển
+    // Bóng chạm vào tường
     public void Change_Direction() {
-
+        if (x < 0 || x > main.WORLD_W - main.RADIUS*2) {
+            Vx = -Vx;
+        }
+        if (y < 0 || y > main.WORLD_H - main.RADIUS*2) {
+            Vy = -Vy;
+            }
+        
     }
+    // Xuất ra màn hình
+    public  void render(SpriteBatch batch) {
+        batch.draw(texture, (float) x, (float) y, main.RADIUS*2, main.RADIUS*2);
+    }
+
+    
+    
+        
+    
 }
