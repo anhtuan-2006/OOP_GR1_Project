6e1d77b015fd8ed1d5e2c548a84bad7509882c43
hanh550 <ngohoanganhltv@gmail.com>
2025-10-11 17:29:45 +0700

    level3

---
+++ core/src/main/java/anhtuannguyen/oop/Level3.java
@@ -0,0 +1,78 @@
+package anhtuannguyen.oop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Level3 {
+    private static final float WORLD_H = Screen.WORLD_H;
+    private static final float WORLD_W = Screen.WORLD_W;
+    private static float Screen_Width;
+    private static float Screen_Height;
+    private Texture background;
+    Ball ball;
+    Bar bar;
+    Block block;
+    boolean playing = true;
+    Play_Pause play_pause;
+
+    private static int[][] map = { // Bản đồ tĩnh: 1 = có khối, 0 = không
+             { 1, 1, 0, 1, 1, 0, 1, 1, 0, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 0, 1, 1, 0, 1, 1, 0, 1, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 1, 0, 1, 1, 0, 1, 1, 0, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 0, 1, 1, 0, 1, 1, 0, 1, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 1, 0, 1, 1, 0, 1, 1, 0, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 0, 1, 1, 0, 1, 1, 0, 1, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    };
+    static int ROW = map.length;
+    static int COL = map[0].length;
+
+    Level3(Play_Pause _play_pause, float Screen_Width, float Screen_Height) {
+        this.Screen_Width = Screen_Width;
+        this.Screen_Height = Screen_Height;
+        play_pause = _play_pause;
+    }
+
+    public void create() {
+        bar = new Bar(WORLD_W / 2 - 150, 200, 300, 50, new Texture("bar_Level3.png"));
+
+        ball = new Ball(bar, new Texture("ball_Level3.png"));
+        
+        block = new Block(0, 0, ball, ROW, COL, map, (int) WORLD_W/COL, 64);
+        block.initializeBlocks(3);
+        background = new Texture("background_Level3.png");
+        if (background == null || bar == null || ball == null || block == null) {
+            System.out.println("Failed to load texture!");
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        if (playing != play_pause.isPlaying()) {
+            playing = !playing;
+            bar.isPlaying();
+            ball.isPlaying();
+        }
+
+        if (!ball.Move()) {
+            Gdx.app.exit();
+        }
+
+        block.checkAndHandleCollisions((float) ball.getx(), (float) ball.gety(), ball.getRADIUS());
+        batch.draw(background, 0, 0, WORLD_W, WORLD_H);
+        ball.render(batch);
+        bar.render(batch);
+        block.renderBlocks(batch);
+    }
+
+    public void dispose() {
+        bar.dispose();
+        block.dispose();
+        ball.dispose();
+    }
+}
